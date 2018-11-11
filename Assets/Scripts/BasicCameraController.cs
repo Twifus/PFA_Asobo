d9bddc18d4970296c205dc6950e4b80a47fa4b93
Jean Farines <moukira45@gmail.com>
2018-11-11 16:18:37 +0100

    Uniformisation des noms de variables
    Standardisation des noms + corrections diverses pour adapter ce changement

--- Assets/Scripts/BasicCameraController.cs
+++ Assets/Scripts/BasicCameraController.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class BasicCameraController : MonoBehaviour {
 
     public GameObject target;
-    private Vector3 offset;
+    private Vector3 _offset;
 
 	// Use this for initialization
 	void Start () {
-		offset = this.transform.position - target.transform.position;
+		_offset = this.transform.position - target.transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.position = target.transform.position + offset;
+        this.transform.position = target.transform.position + _offset;
 	}
 }
--- Assets/Scripts/BasicPlaneController.cs
+++ Assets/Scripts/BasicPlaneController.cs
@@ -5,63 +5,61 @@ using UnityEngine;
 public class BasicPlaneController : MonoBehaviour {
 
     [Range(0f, 100f)]
-    public float wing_area;
+    public float wingArea;
 
     [Range(0f, 100f)]
-    public float lift_coeff;
+    public float liftCoeff;
 
     [Range(0f, 100f)]
-    public float drag_coeff;
+    public float dragCoeff;
 
     [Range(0f, 100f)]
-    public float thrust_power;
+    public float thrustPower;
 
-    [Range(0f, 100f)]
-    public float Yaw_Intensity;
+    public float thrustCoeff;
 
-    [Range(0f, 100f)]
-    public float Pitch_Intensity;
+    [Range(0f, 360f)]
+    public float rollIntensity;
 
+    [Range(0f, 360f)]
+    public float pitchIntensity;
 
-    public float thrust_coeff;
-
-    private Rigidbody rb;
-    private float air_density = 1.184f;
-
-    private int last_heigth;
+    private Rigidbody _rb;
+    private float _airDensity = 1.184f;
+    private int _lastHeigth;
 
     // Use this for initialization
     void Start () {
-        rb = GetComponent<Rigidbody>();
-        last_heigth = 0;
+        _rb = GetComponent<Rigidbody>();
+        _lastHeigth = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.transform.position.y >= last_heigth + 100)
+        if (this.transform.position.y >= _lastHeigth + 100)
         {
-            last_heigth = last_heigth + 100;
-            Debug.Log("Altitude : " + last_heigth);
+            _lastHeigth = _lastHeigth + 100;
+            Debug.Log("Altitude : " + _lastHeigth);
         }
         
-        Vector3 lift = new Vector3(0, 0.5f * lift_coeff * air_density * Mathf.Pow(rb.velocity.x, 2), 0);
-        rb.AddForce(lift);
+        Vector3 lift = new Vector3(0, 0.5f * liftCoeff * _airDensity * Mathf.Pow(_rb.velocity.x, 2), 0);
+        _rb.AddForce(lift);
 
-        Vector3 drag = -0.5f * drag_coeff * air_density * Mathf.Pow(rb.velocity.x, 2) * rb.velocity.normalized;
-        rb.AddForce(drag);
+        Vector3 drag = -0.5f * dragCoeff * _airDensity * Mathf.Pow(_rb.velocity.x, 2) * _rb.velocity.normalized;
+        _rb.AddForce(drag);
 
-        Vector3 thrust = thrust_power * thrust_coeff * this.transform.right;
-        rb.AddForce(thrust);
+        Vector3 thrust = thrustPower * thrustCoeff * this.transform.right;
+        _rb.AddForce(thrust);
 
         if (Input.GetButton("Roll"))
         {
-            this.transform.Rotate(Vector3.forward * Time.deltaTime* Yaw_Intensity * Input.GetAxis("Roll")); //On doit l'enlever à long terme
-            //this.transform.Rotate(Vector3.right * Time.deltaTime * Yaw_Intensity * -Input.GetAxis("Roll")); // C'est ça qui doit nous faire tourner à long terme
+            this.transform.Rotate(Vector3.forward * Time.deltaTime* rollIntensity * Input.GetAxis("Roll")); //On doit l'enlever à long terme
+            //this.transform.Rotate(Vector3.right * Time.deltaTime * rollIntensity * -Input.GetAxis("Roll")); // C'est ça qui doit nous faire tourner à long terme
         }
 
         if (Input.GetButton("Pitch"))
         {
-            this.transform.Rotate(Vector3.up * Time.deltaTime * Pitch_Intensity * Input.GetAxis("Pitch"));
+            this.transform.Rotate(Vector3.up * Time.deltaTime * pitchIntensity * Input.GetAxis("Pitch"));
         }
     }
 }
