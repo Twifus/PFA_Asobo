08e5266661f83b34bc7551c9a2b6b0b1c5be323f
Jean Farines <moukira45@gmail.com>
2018-11-11 14:11:05 +0100

    Organisation du dossier Assets + Test physique de base
    - Nouvelle hierarchie pour bien organiser les différents assets (modèles, scripts, prefab, etc)
    - Scène de test pour une physique de vol basique : Traction + Portance + Trainee + Poids avec des coefficients constants, pas de rotations

---
+++ Assets/Scripts/BasicCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCameraController : MonoBehaviour {
+
+    public GameObject target;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = this.transform.position - target.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = target.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/BasicPlaneController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlaneController : MonoBehaviour {
+
+    [Range(0f, 100f)]
+    public float wing_area;
+
+    [Range(0f, 100f)]
+    public float lift_coeff;
+
+    [Range(0f, 100f)]
+    public float drag_coeff;
+
+    [Range(0f, 100f)]
+    public float thrust_power;
+    
+    public float thrust_coeff;
+
+    private Rigidbody rb;
+    private float air_density = 1.184f;
+
+    private int last_heigth;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        last_heigth = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.position.y >= last_heigth + 100)
+        {
+            last_heigth = last_heigth + 100;
+            Debug.Log("Altitude : " + last_heigth);
+        }
+        
+        Vector3 lift = new Vector3(0, 0.5f * lift_coeff * air_density * Mathf.Pow(rb.velocity.x, 2), 0);
+        rb.AddForce(lift);
+
+        Vector3 drag = -0.5f * drag_coeff * air_density * Mathf.Pow(rb.velocity.x, 2) * rb.velocity.normalized;
+        rb.AddForce(drag);
+
+        Vector3 thrust = new Vector3(thrust_power * thrust_coeff, 0, 0);
+        rb.AddForce(thrust);
+	}
+}
