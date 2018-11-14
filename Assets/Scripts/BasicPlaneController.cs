52f1139902fee1cabd804fbb09679bd7713970c9
Jean Farines <moukira45@gmail.com>
2018-11-14 17:06:43 +0100

    Amelioration de la physique de l'avion
    Ajout de rotations sur les 3 axes
    Coefficient de portance dépendant de l'orientation de l'avion
    Modification des parametres des forces

--- Assets/Scripts/BasicPlaneController.cs
+++ Assets/Scripts/BasicPlaneController.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class BasicPlaneController : MonoBehaviour {
 
     [Range(0f, 100f)]
-    public float wingArea;
+    public float WingArea;
 
     [Range(0f, 100f)]
-    public float liftCoeff;
+    public float LiftCoeff;
 
     [Range(0f, 100f)]
-    public float dragCoeff;
+    public float DragCoeff;
 
     [Range(0f, 100f)]
-    public float thrustPower;
+    public float ThrustPower;
 
-    public float thrustCoeff;
+    public float ThrustCoeff;
 
     [Range(0f, 360f)]
-    public float rollIntensity;
+    public float RollIntensity;
 
     [Range(0f, 360f)]
-    public float pitchIntensity;
+    public float PitchIntensity;
+
+    [Range(0f, 360f)]
+    public float YawIntensity;
 
     private Rigidbody _rb;
     private float _airDensity = 1.184f;
@@ -36,30 +39,33 @@ public class BasicPlaneController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.transform.position.y >= _lastHeigth + 100)
+        if (transform.position.y >= _lastHeigth + 100)
         {
             _lastHeigth = _lastHeigth + 100;
             Debug.Log("Altitude : " + _lastHeigth);
         }
-        
-        Vector3 lift = new Vector3(0, 0.5f * liftCoeff * _airDensity * Mathf.Pow(_rb.velocity.x, 2), 0);
-        _rb.AddForce(lift);
 
-        Vector3 drag = -0.5f * dragCoeff * _airDensity * Mathf.Pow(_rb.velocity.x, 2) * _rb.velocity.normalized;
-        _rb.AddForce(drag);
+        float dynamicLiftCoeff = LiftCoeff * transform.forward.y;
 
-        Vector3 thrust = thrustPower * thrustCoeff * this.transform.right;
-        _rb.AddForce(thrust);
+        //Debug.Log(dynamicLiftCoeff);
+
+        Vector3 lift = 0.5f * dynamicLiftCoeff * _airDensity * _rb.velocity.sqrMagnitude * transform.forward;
+        Vector3 drag = -0.5f * DragCoeff * _airDensity * _rb.velocity.sqrMagnitude * _rb.velocity.normalized;
+        Vector3 thrust = ThrustPower * ThrustCoeff * transform.right;
 
         if (Input.GetButton("Roll"))
-        {
-            this.transform.Rotate(Vector3.forward * Time.deltaTime* rollIntensity * Input.GetAxis("Roll")); //On doit l'enlever à long terme
-            //this.transform.Rotate(Vector3.right * Time.deltaTime * rollIntensity * -Input.GetAxis("Roll")); // C'est ça qui doit nous faire tourner à long terme
-        }
+            transform.Rotate(Vector3.right * Time.deltaTime * RollIntensity * -Input.GetAxis("Roll"));
 
         if (Input.GetButton("Pitch"))
-        {
-            this.transform.Rotate(Vector3.up * Time.deltaTime * pitchIntensity * Input.GetAxis("Pitch"));
-        }
+            transform.Rotate(Vector3.up * Time.deltaTime * PitchIntensity * -Input.GetAxis("Pitch"));
+        
+        if (Input.GetButton("Yaw"))
+            transform.Rotate(Vector3.forward * Time.deltaTime * YawIntensity * Input.GetAxis("Yaw"));
+        
+        thrust = thrust * Input.GetAxis("Accelerate");
+
+        _rb.AddForce(lift);
+        _rb.AddForce(drag);
+        _rb.AddForce(thrust);
     }
 }
