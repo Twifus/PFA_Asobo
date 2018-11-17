9148f62540af14ec35b3cc79c935358ea94ac224
natoles <lindien.martin@gmail.com>
2018-11-17 21:29:46 +0100

    Agrandissement terrain + ajout d'un ring pour le FUN  + musique d'ambiance. WORK IN PROGRESS

---
+++ Assets/RingPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingPoint : MonoBehaviour {
+
+    //BoxCollider Cld;
+
+
+	// Use this for initialization
+	void Start () {
+        //Cld = GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("1 point");
+    }
+}
