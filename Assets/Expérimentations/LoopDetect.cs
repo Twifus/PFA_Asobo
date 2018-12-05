f1dfb847ff56258b835f994b93da65e7d3679b6e
Anatole <lindien.martin@gmail.com>
2018-12-05 13:53:37 +0100

    Ajout UI quand looping effectué

--- Assets/Expérimentations/LoopDetect.cs
+++ Assets/Expérimentations/LoopDetect.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class LoopDetect : MonoBehaviour {
 
-    public GameObject plane;
+    public GameObject Plane;
+    public bool LoopDone = false;
 
     private int state = 0;
 
@@ -15,41 +16,45 @@ public class LoopDetect : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (plane.transform.eulerAngles.z <= 20 && (state == 0 || state == 1))
+
+        LoopDone = false;
+
+		if (Plane.transform.eulerAngles.z <= 20 && (state == 0 || state == 1))
         {
             Debug.Log("Loop : 1");
             state = 1;
         }
         else
         {
-            if (20 < plane.transform.eulerAngles.z && plane.transform.eulerAngles.z <= 100 && (state == 1 || state == 2))
+            if (20 < Plane.transform.eulerAngles.z && Plane.transform.eulerAngles.z <= 100 && (state == 1 || state == 2))
             {
                 Debug.Log("Loop : 2");
                 state = 2;
             }
             else
             {
-                if (100 < plane.transform.eulerAngles.z && plane.transform.eulerAngles.z <= 200 && (state == 2 || state == 3))
+                if (100 < Plane.transform.eulerAngles.z && Plane.transform.eulerAngles.z <= 200 && (state == 2 || state == 3))
                 {
                     Debug.Log("Loop : 3");
                     state = 3;
                 }
                 else
                 {
-                    if (200 < plane.transform.eulerAngles.z && plane.transform.eulerAngles.z <= 350 && (state == 3 || state == 4))
+                    if (200 < Plane.transform.eulerAngles.z && Plane.transform.eulerAngles.z <= 350 && (state == 3 || state == 4))
                     {
                         Debug.Log("Loop : 4");
                         state = 4;
                     }
                     else
                     {
-                        if (350 <= plane.transform.eulerAngles.z && state == 4)
+                        if (350 <= Plane.transform.eulerAngles.z && state == 4)
                         {
                             Debug.Log("LOOOOOP");
                             Debug.Log("LOOOOOP");
                             Debug.Log("LOOOOOP");
                             Debug.Log("LOOOOOP");
                             state = 0;
+                            LoopDone = true;
                         }
                         else
                         {
