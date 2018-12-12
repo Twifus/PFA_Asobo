8b193d4aba252e366b93839dae3102929714affe
FelixPierre <44431537+FelixPierre@users.noreply.github.com>
2018-12-12 14:20:44 +0100

    class Plane

--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -2,15 +2,108 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plane : MonoBehaviour {
+public class Plane {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float _wingArea;
+    private float _liftCoeff;
+    private float _dragCoeff;
+    private float _trustPower;
+    private float _trustCoeff;
+    private float _rollIntensity;
+    private float _pitchIntensity;
+    private float _yawIntensity;
+
+    private GameObject _plane;
+    private Rigidbody _rigidbody;
+
+
+    #region Properties
+
+    public float WingArea {
+        get {
+            return _wingArea;
+        }
+    }
+
+    public float LiftCoeff {
+        get {
+            return _liftCoeff;
+        }
+    }
+
+    public float DragCoeff {
+        get {
+            return _dragCoeff;
+        }
+    }
+
+    public float TrustPower {
+        get {
+            return _trustPower;
+        }
+    }
+
+    public float TrustCoeff {
+        get {
+            return _trustCoeff;
+        }
+    }
+
+    public float RollIntensity {
+        get {
+            return _rollIntensity;
+        }
+    }
+
+    public float PitchIntensity {
+        get {
+            return _pitchIntensity;
+        }
+    }
+
+    public float YawIntensity {
+        get {
+            return _yawIntensity;
+        }
+    }
+
+    #endregion
+
+
+    #region Constructor
+
+    public Plane(GameObject plane) {
+        _plane = plane;
+        _rigidbody = plane.GetComponent<Rigidbody>();
+        LoadSettings();
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    public void AddForce(Vector3 force) {
+        _rigidbody.AddForce(force);
+    }
+
+
+    public void LoadSettings() {
+
+    }
+
+    public void LoadSettings(float wingArea, float liftCoeff, float dragCoeff,
+                            float thrustPower, float thrustCoeff, float rollIntensity,
+                            float pitchIntensity, float yawIntensity) {
+        _wingArea = wingArea;
+        _liftCoeff = liftCoeff;
+        _dragCoeff = dragCoeff;
+        _trustPower = thrustPower;
+        _trustCoeff = thrustCoeff;
+        _rollIntensity = rollIntensity;
+        _pitchIntensity = pitchIntensity;
+        _yawIntensity = yawIntensity;
+    }
+
+    #endregion
 }
