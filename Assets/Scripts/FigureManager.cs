f1ff4523ecd5df68d61d0676e83739ff639c1f73
ATKSkor <cesarolouis@gmail.com>
2018-12-12 16:01:44 +0100

    FigureManager
    Ajout du FigureManager.cs définition de certaines fonctions

---
+++ Assets/Scripts/FigureManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FigureManager : MonoBehaviour{
+    public GameObject plane;
+    public FigureDetection figureDetection;
+    public Settings settings;
+
+    private int _score;
+
+    /** Récupère les coordonnées et les rotations de l'avion dans un tableau (ArrayList à voir) */
+    private ArrayList GetCoordinates(GameObject plane)
+    {
+        ArrayList array = new ArrayList();
+        return array;
+    }
+
+    /** Affiche le score du joueur */
+    private void DisplayScore(int points)
+    {
+        //TODO
+    }
+
+    /** Met à jour le score du joueur */
+    private void UpdateScore(int points)
+    {
+        _score += points;
+    }
+
+    /** Appelle la fonction qui analyse la trajectoire */
+    private void AnalyzeTrajectory()
+    {
+        //TODO
+    }
+
+    /** Détermine quelle figure a été réalisée */
+    private void WhichFigure()
+    {
+        //TODO
+    }
+}
