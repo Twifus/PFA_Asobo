e603eb5b343737a5a5c661ce616c07a9c74dda13
Anatole <lindien.martin@gmail.com>
2018-12-12 16:07:10 +0100

    AJout de l'automate pour detecter loop
    (n'affiche rien pour l'instant)

---
+++ Assets/Scripts/FsmDetectionUpdate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+public class FsmDetectionUpdate : MonoBehaviour {
+
+    public PlayMakerFSM Fsm;
+    public GameObject Plane;
+
+    // Update is called once per frame
+    void Update () {
+        // getting fsm variables by name
+        FsmFloat PlaneRot = Fsm.FsmVariables.GetFsmFloat("PlaneRot");
+
+        // setting fsm variable value
+        float pitch = Vector3.Angle(Vector3.up, Plane.transform.up);
+        if (Plane.transform.right.y < 0)
+        {
+            pitch = 360 - pitch;
+        }
+        PlaneRot.Value = pitch; // Plane.transform.eulerAngles.z;
+        Debug.Log(PlaneRot.Value);
+
+        // sending an event
+        //fsm.SendEvent("myEvent");
+
+    }
+}
