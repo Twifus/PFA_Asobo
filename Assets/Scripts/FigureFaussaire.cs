0d4fcae2f3f04b333d86871b03ebe3ff108013aa
ATKSkor <cesarolouis@gmail.com>
2019-02-05 10:24:40 +0100

    Ajout de la classe FigureFaussaire pour test

---
+++ Assets/Scripts/FigureFaussaire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FigureFaussaire{
+    private float timeBarrel;
+    private float timeLoop;
+
+
+	// Use this for initialization
+	void Start () {
+        timeBarrel = Time.time;
+        timeLoop = timeBarrel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
