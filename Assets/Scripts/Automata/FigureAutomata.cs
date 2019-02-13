e43b81d5e9f04b846703ee2d207bd9446c840a6a
Timothée Corsini <timothee.corsini@gmail.com>
2019-02-13 15:36:34 +0100

    compilation ok

--- Assets/Scripts/Automata/FigureAutomata.cs
+++ Assets/Scripts/Automata/FigureAutomata.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 
 //positions en transform, ensemble des points sur lesquels tournent les automates et $1
-public struct Coordinate {int x;}
+public struct Coordinate {
+    //public int x;
+}
 //ensemble des figures : Looping, Barel Roll, Cuban 8, double looping etc...
-enum FigureId {A,B,C}
+public enum FigureId {A,B,C}
 //représente une stat sur une figure : son pourcentage de reconnaissance
 public struct FigureDetection {
     FigureId id; 
     int p; 
-    FigureDetection(FigureId id, int p) {
+    public FigureDetection(FigureId id, int p) {
         this.id = id;
         this.p = p;
     }
@@ -63,16 +65,19 @@ public abstract class AbstractComposedFigureAutomata : IFigureAutomata {
         _subFigures.Add(auto);
         _subFiguresRealised.Add(false);
     }
-    void resetStates() {
+    public abstract FigureId getFigureId();
+    public abstract string getName();
+    public void resetStates() {
         _mainFigure.resetStates();
-        foreach(IFigureAutomata auto in _subFigures)
+        int i = 0;
+        foreach(IFigureAutomata auto in _subFigures) {
             auto.resetStates();
-        foreach(bool b in _subFiguresRealised)
-            b = false; //à refaire
+            _subFiguresRealised[i++] = false;
+        }
     }
-    bool isValid() {
+    public bool isValid() {
         if(_mainFigure.isValid()) {
-            foreach(bool b in _subFiguresRealised) {
+            foreach(var b in _subFiguresRealised) {
                 if(!b)
                     return false;
             }
@@ -81,20 +86,20 @@ public abstract class AbstractComposedFigureAutomata : IFigureAutomata {
         else
             return false;
     }
-    int getCurrentState() {
+    public int getCurrentState() {
         if (isValid())
             return 1;
         return 0;
     }
-    int getNumberOfState() {return 1;}
-    int calculateState(Coordinate newPos) {
+    public int getNumberOfState() {return 1;}
+    public int calculateState(Coordinate newPos) {
         int mainState = _mainFigure.calculateState(newPos);
         if (mainState == -1)
             resetStates(); //on relance tout
         else { //cas normal
             int i = 0;
             foreach(IFigureAutomata auto in _subFigures) {
-                if(_subFigures.calculateState(newPos) == 1)
+                if(auto.calculateState(newPos) == 1)
                     _subFiguresRealised[i] = true;
                 i++;
             }
@@ -114,33 +119,35 @@ public abstract class AbstractSequentialFigureAutomata : IFigureAutomata {
     protected List<IFigureAutomata> _listFigures;
     protected int _currentFigureIndex;
     //reinitialise l'automate de la figure
-    IFigureAutomata getCurrentAuto() {
+    public IFigureAutomata getCurrentAuto() {
         return _listFigures[_currentFigureIndex];
     }
-    void resetStates() {
+    public abstract FigureId getFigureId();
+    public abstract string getName();
+    public void resetStates() {
         foreach(IFigureAutomata auto in _listFigures)
             auto.resetStates();
         _currentFigureIndex = 0;
     }
-    bool isValid() {
+    public bool isValid() {
         return (_currentFigureIndex == _listFigures.Count); 
                 //&& getCurrentAuto().isValid());;
     }
     //renvoie l'id de l'état actuel
-    int getCurrentState() {
+    public int getCurrentState() {
         if (isValid())
             return _listFigures.Count;
         return _currentFigureIndex;
     }
     //renvoie le nombre d'états de l'automate
-    int getNumberOfState() {
+    public int getNumberOfState() {
         return _listFigures.Count;
     }
     //calcule le nouvel état de l'automate étant donné la position passée en paramètre
     //renvoie 1 si le nouvel état est terminal (même résultat que isValid())
     //0 si le nouvel état est intermédiaire
     //-1 si l'automate recommence à l'état initial
-    int calculateState(Coordinate newPos) {
+    public int calculateState(Coordinate newPos) {
         if (isValid()) 
             return 1;
         int intState = getCurrentAuto().calculateState(newPos);
@@ -158,13 +165,13 @@ public abstract class AbstractSequentialFigureAutomata : IFigureAutomata {
 }
 
 public class DummyAutomata : IFigureAutomata {
-    void resetStates() {}
-    FigureId getFigureId() {return FigureId.A;}
-    string getName() {return "";}
-    bool isValid() {return true;}
-    int getCurrentState() {return 0;}
-    int getNumberOfState() {return 3;}
-    bool calculateState(Coordinate newPos) {return 0;}
+    public void resetStates() {}
+    public FigureId getFigureId() {return FigureId.A;}
+    public string getName() {return "";}
+    public bool isValid() {return true;}
+    public int getCurrentState() {return 0;}
+    public int getNumberOfState() {return 3;}
+    public int calculateState(Coordinate newPos) {return 0;}
 }
 
 /*
@@ -175,6 +182,7 @@ public class AutomataDetector : IFigureDetection {
     private List<IFigureAutomata> _myAutomatas;
 
     public AutomataDetector(int n) {
+        _myAutomatas = new List<IFigureAutomata>();
         for(int i = 0; i < n; i++)
             _myAutomatas.Add(new DummyAutomata());
     }
@@ -192,7 +200,8 @@ public class AutomataDetector : IFigureDetection {
                 auto.resetStates();
             }
             else
-                list.Add(Figure(auto.getFigureId(), 0));
+                list.Add(new FigureDetection(auto.getFigureId(), 0));
         }
+        return list;
     }
 }
