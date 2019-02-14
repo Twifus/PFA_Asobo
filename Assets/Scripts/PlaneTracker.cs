7b43bc424b0e2f70d683faea02eb4d7a614c044e
Jean Farines <moukira45@gmail.com>
2019-02-14 18:19:30 +0100

    Ajout d'un tracker qui écrit la trajectoire de l'avion dans un fichier csv

---
+++ Assets/Scripts/PlaneTracker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PlaneTracker : MonoBehaviour {
+
+    private StreamWriter Writer;
+    private Plane Plane;
+    public GameObject Player;
+
+    public void Start()
+    {
+        string path = string.Format("../Figure-{0}.csv", System.DateTime.Now.ToFileTime());
+        Writer = new StreamWriter(path, true);
+        Plane = Plane.NewPlane(Player);
+        Debug.Log("Writting in " + path);
+    }
+
+    private void OnApplicationQuit()
+    {
+        Writer.Close();
+    }
+
+    private void Update()
+    {
+        //Debug.Log(string.Format("{0},{1},{2},{3},{4}", Time.time, Plane.Position.z, Plane.Roll, Plane.Pitch, Plane.Yaw));
+        Writer.WriteLine(string.Format("{0},{1},{2},{3},{4}", Time.time, Plane.Position.y, Plane.Roll, Plane.Pitch, Plane.Yaw));
+    }
+
+}
