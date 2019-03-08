cedc43329ff49b9e299b2ffbd6d8007484d93868
Anatole <lindien.martin@gmail.com>
2019-03-08 09:06:30 +0100

    flavour text changed

--- Assets/Scripts/Automata/Program.cs
+++ Assets/Scripts/Automata/Program.cs
@@ -6,14 +6,6 @@ public class Program
     static void Main(string[] args)
     {
         LoopingAutomata l = new LoopingAutomata();
-        /* 
-        Console.WriteLine("Current State = " + l.CurrentState);
-        Console.WriteLine("Transition.todX90: Current State = " +(LoopingState) l.MoveNext((int) LoopingTransition.todX90));
-        Console.WriteLine("Transition.todX180: Current State = " +(LoopingState) l.MoveNext((int) LoopingTransition.todX180));
-        Console.WriteLine("Transition.todX270: Current State = " +(LoopingState) l.MoveNext((int) LoopingTransition.todX270));
-        Console.WriteLine("Transition.todX360: Current State = " +(LoopingState) l.MoveNext((int) LoopingTransition.todX360));
-        Console.WriteLine("Transition.Reset: Current State = " +(LoopingState) l.MoveNext((int) LoopingTransition.Reset));
-        */
         Coordinate simu = new Coordinate();
         List<int> trajectoryX = new List<int>();
         trajectoryX.Add(120);
@@ -24,6 +16,7 @@ public class Program
         trajectoryX.Add(200);
         trajectoryX.Add(300);
         trajectoryX.Add(10);
+        trajectoryX.Add(100);
         trajectoryX.Add(100);
 
         Console.WriteLine("LOOPING :");
@@ -50,18 +43,6 @@ public class Program
             a.calculateState(simu);
             Console.WriteLine("Current State ARoll = " + a.CurrentState);
         }
-
-
-
-    /* 
-        ARollAutomata a = new ARollAutomata();
-        Console.WriteLine("Current State = " + a.CurrentState);
-        Console.WriteLine("Transition.todZ90: Current State = " +(ARollState) a.MoveNext((int) ARollTransition.todZ90));
-        Console.WriteLine("Transition.todZ180: Current State = " +(ARollState) a.MoveNext((int) ARollTransition.todZ180));
-        Console.WriteLine("Transition.todZ270: Current State = " +(ARollState) a.MoveNext((int) ARollTransition.todZ270));
-        Console.WriteLine("Transition.todZ360: Current State = " +(ARollState) a.MoveNext((int) ARollTransition.todZ360));
-        Console.WriteLine("Transition.Reset: Current State = " +(ARollState) a.MoveNext((int) ARollTransition.Reset));
-    */
     }
     
 }
