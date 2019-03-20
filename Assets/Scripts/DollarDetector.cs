bbde79d58d092536c194bf82bffde7be42b7f8b3
Adrionche <adrionche@gmail.com>
2019-03-20 19:21:22 +0100

    implémentation de listes dans $p

--- Assets/Scripts/DollarDetector.cs
+++ Assets/Scripts/DollarDetector.cs
@@ -7,44 +7,42 @@ using PDollarGestureRecognizer;
 
 public class DollarDetector : IFigureDetection {
 
-    private int MAX_SIZE = 200;
-    private int next = -1;
+    private int MAX_SIZE = 120;
     private int time = 0;
 
-    Point[] _timePointsHeight;
-    Point[] _timePointsRoll;
-    Point[] _timePointsPitch;
-    Point[] _timePointsYaw;
+    List<Point> _timePointsHeight;
+    List<Point> _timePointsRoll;
+    List<Point> _timePointsPitch;
+    List<Point> _timePointsYaw;
 
-    Gesture[] gesturesHeight;
-    Gesture[] gesturesRoll;
-    Gesture[] gesturesPitch;
-    Gesture[] gesturesYaw;
+    List<Gesture> gesturesHeight;
+    List<Gesture> gesturesRoll;
+    List<Gesture> gesturesPitch;
+    List<Gesture> gesturesYaw;
 
 
     public DollarDetector()
     {
-        gesturesHeight = new Gesture[3];
-        gesturesRoll = new Gesture[3];
-        gesturesPitch = new Gesture[3];
-        gesturesYaw = new Gesture[2];
+        gesturesHeight = new List<Gesture>();
+        gesturesRoll = new List<Gesture>();
+        gesturesPitch = new List<Gesture>();
+        gesturesYaw = new List<Gesture>();
         FigureLoaderP loader = new FigureLoaderP();
         loader.LoadFigures(gesturesHeight, gesturesRoll, gesturesPitch, gesturesYaw);
 
-        _timePointsHeight = new Point[MAX_SIZE];
-        _timePointsRoll = new Point[MAX_SIZE];
-        _timePointsPitch = new Point[MAX_SIZE];
-        _timePointsYaw = new Point[MAX_SIZE];
+        _timePointsHeight = new List<Point>();
+        _timePointsRoll = new List<Point>();
+        _timePointsPitch = new List<Point>();
+        _timePointsYaw = new List<Point>();
     }
 
 
     public void setPoint(Coordinate point) {
-        if (next == MAX_SIZE - 1)
+        if (_timePointsHeight.Count == MAX_SIZE)
         {
             int i;
-            for (i = 0; i < MAX_SIZE - 2; i++)
+            for (i = 0; i < MAX_SIZE - 1; i++)
             {
-
                 _timePointsHeight[i] = _timePointsHeight[i + 1];
                 _timePointsRoll[i] = _timePointsRoll[i + 1];
                 _timePointsPitch[i] = _timePointsPitch[i + 1];
@@ -56,73 +54,70 @@ public class DollarDetector : IFigureDetection {
                 _timePointsYaw[i].X--;
             }
         }
-        else
-        {
-            next++;
-        }
         time++;
-        _timePointsHeight[next] = new Point(time, point.ypos, 0);
-        _timePointsRoll[next] = new Point(time, point.roll, 0);
-        _timePointsPitch[next] = new Point(time, point.pitch, 0);
-        _timePointsYaw[next] = new Point(time, point.yaw, 0);
+        _timePointsHeight.Add(new Point(time, point.ypos, 0));
+        _timePointsRoll.Add(new Point(time, point.roll, 0));
+        _timePointsPitch.Add(new Point(time, point.pitch, 0));
+        _timePointsYaw.Add(new Point(time, point.yaw, 0));
     }
 
     private void ClearLists()
     {
-        next = -1;
+        _timePointsHeight.Clear();
+        _timePointsRoll.Clear();
+        _timePointsPitch.Clear();
+        _timePointsYaw.Clear();
         time = 0;
     }
 
     private bool AnalyseResults(BestGesture resultHeight, BestGesture resultRoll, BestGesture resultPitch, BestGesture resultYaw, string height, string roll, string pitch, string yaw)
     {
-        return (resultHeight.Name.Equals(height) //&& resultHeight.Score > 0.7f
+        return (resultHeight.Name.Equals(height) && resultHeight.Score > 0.7f
             && resultRoll.Name.Equals(roll) //&& resultRoll.Score > 0.7f
-            && resultPitch.Name.Equals(pitch) //&& resultPitch.Score > 0.7f
-            && resultYaw.Name.Equals(yaw)); //&& resultYaw.Score > 0.7f);
+            && resultPitch.Name.Equals(pitch) && resultPitch.Score > 0.7f
+            && resultYaw.Name.Equals(yaw) && resultYaw.Score > 0.7f);
     }
 
 	public List<Figure> detection() {
         List<Figure> result = new List<Figure>();
+        BestGesture resultHeight = PointCloudRecognizer.Classify(new Gesture(_timePointsHeight.ToArray(), "test height"), gesturesHeight.ToArray());
+        BestGesture resultRoll = PointCloudRecognizer.Classify(new Gesture(_timePointsRoll.ToArray(), "test roll"), gesturesRoll.ToArray());
+        BestGesture resultPitch = PointCloudRecognizer.Classify(new Gesture(_timePointsPitch.ToArray(), "test pitch"), gesturesPitch.ToArray());
+        BestGesture resultYaw = PointCloudRecognizer.Classify(new Gesture(_timePointsYaw.ToArray(), "test yax"), gesturesYaw.ToArray());
 
-        if (next == MAX_SIZE - 1)
+        //Debug.Log(resultHeight.Name + ", " + resultRoll.Name + ", " + resultPitch.Name + ", " + resultYaw.Name);
+
+
+        // Loop
+        if (AnalyseResults(resultHeight, resultRoll, resultPitch, resultYaw, "Bosse", "LigneCoupee", "ZigZag", "LigneCoupee"))
         {
-            BestGesture resultHeight = PointCloudRecognizer.Classify(new Gesture(_timePointsHeight, "test height"), gesturesHeight);
-            BestGesture resultRoll = PointCloudRecognizer.Classify(new Gesture(_timePointsRoll, "test roll"), gesturesRoll);
-            BestGesture resultPitch = PointCloudRecognizer.Classify(new Gesture(_timePointsPitch, "test pitch"), gesturesPitch);
-            BestGesture resultYaw = PointCloudRecognizer.Classify(new Gesture(_timePointsYaw, "test yax"), gesturesYaw);
+            Debug.Log("Loop");
+            Debug.Log(resultHeight.Score + ", " + resultRoll.Score + ", " + resultPitch.Score + ", " + resultYaw.Score);
+            result.Add(new Figure());
+            result[0].id = figure_id.LOOP;
+            result[0].quality = 1f;
+            ClearLists();
+        }
 
-            //Debug.Log(resultHeight.Name + ", " + resultRoll.Name + ", " + resultPitch.Name + ", " + resultYaw.Name);
+        // Roll
+        if (AnalyseResults(resultHeight, resultRoll, resultPitch, resultYaw, "LigneDroite", "LigneMontante", "LigneDroite", "LigneDroite"))
+        {
+            Debug.Log("Roll");
+            Debug.Log(resultHeight.Score + ", " + resultRoll.Score + ", " + resultPitch.Score + ", " + resultYaw.Score);
+            result.Add(new Figure());
+            result[0].id = figure_id.BARREL;
+            result[0].quality = 1f;
+            ClearLists();
+        }
 
-
-            // Loop
-            if (AnalyseResults(resultHeight, resultRoll, resultPitch, resultYaw, "Bosse", "LigneCoupee", "ZigZag", "LigneCoupee"))
-            {
-                Debug.Log("Loop");
-                result.Add(new Figure());
-                result[0].id = figure_id.LOOP;
-                result[0].quality = 1f;
-                ClearLists();
-            }
-
-            // Roll
-            if (AnalyseResults(resultHeight, resultRoll, resultPitch, resultYaw, "LigneDroite", "LigneMontante", "LigneDroite", "LigneDroite"))
-            {
-                Debug.Log("Roll");
-                result.Add(new Figure());
-                result[0].id = figure_id.BARREL;
-                result[0].quality = 1f;
-                ClearLists();
-            }
-
-            // Cuban Eight
-            if (AnalyseResults(resultHeight, resultRoll, resultPitch, resultYaw, "DoubleBosse", "DoubleDemieLigneMontante", "DoubleZigZag", "LigneCoupee"))
-            {
-                Debug.Log("CubanEight");
-                result.Add(new Figure());
-                result[0].id = figure_id.CUBANEIGHT;
-                result[0].quality = 1f;
-                ClearLists();
-            }
+        // Cuban Eight
+        if (AnalyseResults(resultHeight, resultRoll, resultPitch, resultYaw, "DoubleBosse", "DoubleDemieLigneMontante", "DoubleZigZag", "LigneCoupee"))
+        {
+            Debug.Log("CubanEight");
+            result.Add(new Figure());
+            result[0].id = figure_id.CUBANEIGHT;
+            result[0].quality = 1f;
+            ClearLists();
         }
         return result;
 	}
--- Assets/Scripts/DollarP/FigureLoaderP.cs
+++ Assets/Scripts/DollarP/FigureLoaderP.cs
@@ -13,17 +13,11 @@ public class FigureLoaderP {
     private string[] figures = { "Roll", "Loop" };
     private string[] curves = { "height", "roll", "pitch", "yaw" };
 
-    private Gesture[] gesturesHeight;
-    private Gesture[] gesturesRoll;
-    private Gesture[] gesturesPitch;
-    private Gesture[] gesturesYaw;
-    private int[] next = { 0, 0, 0, 0 };
-
     #endregion
 
     #region Loader
 
-    public void LoadFigures(Gesture[] height, Gesture[] roll, Gesture[] pitch, Gesture[] yaw) {
+    public void LoadFigures(List<Gesture> height, List<Gesture> roll, List<Gesture> pitch, List<Gesture> yaw) {
         //foreach(string figure in figures) {
         //    foreach(string curve in curves) {
         //        // vérifier l'existence du fichier
@@ -34,24 +28,9 @@ public class FigureLoaderP {
         //    }
         //}
 
-        gesturesHeight = new Gesture[3];
-        gesturesRoll = new Gesture[3];
-        gesturesPitch = new Gesture[3];
-        gesturesYaw = new Gesture[2];
-
-        Loop();
-        Roll();
-        CubanEight();
-
-        for (int i=0; i<3; i++)
-        {
-            height[i] = gesturesHeight[i];
-            roll[i] = gesturesRoll[i];
-            pitch[i] = gesturesPitch[i];
-            if (i < 2) yaw[i] = gesturesYaw[i];
-        }
-
-        
+        Loop(height, roll, pitch, yaw);
+        Roll(height, roll, pitch, yaw);
+        //CubanEight(height, roll, pitch, yaw);
     }
 
     #endregion
@@ -61,43 +40,43 @@ public class FigureLoaderP {
     // Créé tous les fichiers xml associés à la figure donnée
     private void CreateFigureFiles(string figure) {
         StreamReader file = new StreamReader(filePath + figure + ".csv");
-        Point[] height = new Point[256];
-        Point[] roll = new Point[256];
-        Point[] pitch = new Point[256];
-        Point[] yaw = new Point[256];
-        
+        List<Point> height = new List<Point>();
+        List<Point> roll = new List<Point>();
+        List<Point> pitch = new List<Point>();
+        List<Point> yaw = new List<Point>();
+
         // Lire le fichier et le parser
         int i = 0;
         do {
             string textLine = file.ReadLine();
             float[] values = Array.ConvertAll<string, float>(textLine.Split(';'), new Converter<string, float>(StringToFloat));
             long time = (long)(values[0] * 1000); // values[0] est le temps en secondes
-            height[i] = new Point(i, values[1], 0);
-            roll[i] = new Point(i, values[2], 0);
-            pitch[i] = new Point(i, values[3], 0);
-            yaw[i] = new Point(i, values[4], 0);
+            height.Add(new Point(i, values[1], 0));
+            roll.Add(new Point(i, values[2], 0));
+            pitch.Add(new Point(i, values[3], 0));
+            yaw.Add(new Point(i, values[4], 0));
             i++;
         } while (file.Peek() != -1);
         
         // Ecrire les fichiers de figures
-        GestureIO.WriteGesture(height, figure, filePath + figure + "_" + "height" + ".xml");
-        GestureIO.WriteGesture(roll, figure, filePath + figure + "_" + "roll" + ".xml");
-        GestureIO.WriteGesture(pitch, figure, filePath + figure + "_" + "pitch" + ".xml");
-        GestureIO.WriteGesture(yaw, figure, filePath + figure + "_" + "yaw" + ".xml");
+        GestureIO.WriteGesture(height.ToArray(), figure, filePath + figure + "_" + "height" + ".xml");
+        GestureIO.WriteGesture(roll.ToArray(), figure, filePath + figure + "_" + "roll" + ".xml");
+        GestureIO.WriteGesture(pitch.ToArray(), figure, filePath + figure + "_" + "pitch" + ".xml");
+        GestureIO.WriteGesture(yaw.ToArray(), figure, filePath + figure + "_" + "yaw" + ".xml");
     }
 
 
-    private void CSVParser(StreamReader file, Point[] height, Point[] roll, Point[] pitch, Point[] yaw) {
+    private void CSVParser(StreamReader file, List<Point> height, List<Point> roll, List<Point> pitch, List<Point> yaw) {
         // Lire le fichier et le parser
         int i = 0;
         do {
             string textLine = file.ReadLine();
             float[] values = Array.ConvertAll<string, float>(textLine.Split(';'), new Converter<string, float>(StringToFloat));
             long time = (long)(values[0] * 1000); // values[0] est le temps en secondes
-            height[i] = new Point(i, values[1], 0);
-            roll[i] = new Point(i, values[2], 0);
-            pitch[i] = new Point(i, values[3], 0);
-            yaw[i] = new Point(i, values[4], 0);
+            height.Add(new Point(i, values[1], 0));
+            roll.Add(new Point(i, values[2], 0));
+            pitch.Add(new Point(i, values[3], 0));
+            yaw.Add(new Point(i, values[4], 0));
             i++;
         } while (file.Peek() != -1);
     }
@@ -112,57 +91,57 @@ public class FigureLoaderP {
 
     #region Figures
 
-    private void Loop() {
+    private void Loop(List<Gesture> gesturesHeight, List<Gesture> gesturesRoll, List<Gesture> gesturesPitch, List<Gesture> gesturesYaw) {
         StreamReader file = new StreamReader(filePath + "Loop" + "-Traj.csv");
-        Point[] height = new Point[362];
-        Point[] roll = new Point[362];
-        Point[] pitch = new Point[362];
-        Point[] yaw = new Point[362];
+        List<Point> height = new List<Point>();
+        List<Point> roll = new List<Point>();
+        List<Point> pitch = new List<Point>();
+        List<Point> yaw = new List<Point>();
 
         // Lire le fichier et le parser
         CSVParser(file, height, roll, pitch, yaw);
 
         // Créer les figures
-        gesturesHeight[next[0] ++]= new Gesture(height, "Bosse");
-        gesturesRoll[next[1] ++] = new Gesture(roll, "LigneCoupee");
-        gesturesPitch[next[2] ++] = new Gesture(pitch, "ZigZag");
-        gesturesYaw[next[3] ++] = new Gesture(yaw, "LigneCoupee");
+        gesturesHeight.Add(new Gesture(height.ToArray(), "Bosse"));
+        gesturesRoll.Add(new Gesture(roll.ToArray(), "LigneCoupee"));
+        gesturesPitch.Add(new Gesture(pitch.ToArray(), "ZigZag"));
+        gesturesYaw.Add(new Gesture(yaw.ToArray(), "LigneCoupee"));
     }
 
 
-    private void Roll() {
+    private void Roll(List<Gesture> gesturesHeight, List<Gesture> gesturesRoll, List<Gesture> gesturesPitch, List<Gesture> gesturesYaw) {
         StreamReader file = new StreamReader(filePath + "Roll" + "-Traj.csv");
-        Point[] height = new Point[276];
-        Point[] roll = new Point[276];
-        Point[] pitch = new Point[276];
-        Point[] yaw = new Point[276];
+        List<Point> height = new List<Point>();
+        List<Point> roll = new List<Point>();
+        List<Point> pitch = new List<Point>();
+        List<Point> yaw = new List<Point>();
    
         // Lire le fichier et le parser
         CSVParser(file, height, roll, pitch, yaw);
 
         // Créer les figures
-        gesturesHeight[next[0] ++] = new Gesture(height, "LigneDroite");
-        gesturesRoll[next[1] ++] = new Gesture(roll, "LigneMontante");
-        gesturesPitch[next[2] ++] = new Gesture(pitch, "LigneDroite");
-        gesturesYaw[next[3] ++] = new Gesture(yaw, "LigneDroite");
+        gesturesHeight.Add(new Gesture(height.ToArray(), "LigneDroite"));
+        gesturesRoll.Add(new Gesture(roll.ToArray(), "LigneMontante"));
+        gesturesPitch.Add(new Gesture(pitch.ToArray(), "LigneDroite"));
+        gesturesYaw.Add(new Gesture(yaw.ToArray(), "LigneDroite"));
     }
 
 
-    private void CubanEight() {
+    private void CubanEight(List<Gesture> gesturesHeight, List<Gesture> gesturesRoll, List<Gesture> gesturesPitch, List<Gesture> gesturesYaw) {
         StreamReader file = new StreamReader(filePath + "CubanEight" + "-Traj.csv");
-        Point[] height = new Point[1333];
-        Point[] roll = new Point[1333];
-        Point[] pitch = new Point[1333];
-        Point[] yaw = new Point[1333];
+        List<Point> height = new List<Point>();
+        List<Point> roll = new List<Point>();
+        List<Point> pitch = new List<Point>();
+        List<Point> yaw = new List<Point>();
 
         // Lire le fichier et le parser
         CSVParser(file, height, roll, pitch, yaw);
 
         // Créer les figures
-        gesturesHeight[next[0] ++] = new Gesture(height, "DoubleBosse");
-        gesturesRoll[next[1] ++] = new Gesture(roll, "DoubleDemieLigneMontante");
-        gesturesPitch[next[2] ++] = new Gesture(pitch, "DoubleZigZag");
-        //gesturesYaw[next[3]] = new Gesture(yaw, "LigneCoupee");
+        gesturesHeight.Add(new Gesture(height.ToArray(), "DoubleBosse"));
+        gesturesRoll.Add(new Gesture(roll.ToArray(), "DoubleDemieLigneMontante"));
+        gesturesPitch.Add(new Gesture(pitch.ToArray(), "DoubleZigZag"));
+        //gesturesYaw.Add(new Gesture(yaw.ToArray(), "LigneCoupee"));
     }
 
     #endregion
