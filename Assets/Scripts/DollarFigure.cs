38ff4ca13bee61ae1ecc288c9ea11fa07e80f903
FelixPierre <44431537+FelixPierre@users.noreply.github.com>
2019-04-05 14:18:52 +0200

    update du cuban-eight pour dollar

--- Assets/Scripts/DollarFigure.cs
+++ Assets/Scripts/DollarFigure.cs
@@ -7,12 +7,13 @@ public static class DollarFigure
 
     // Files loading variables
     public static readonly string filePath = "./Assets/Figures/";
+    // Y, pRight.wUp, pUp.wUp, pForward.wUp
     public static readonly int[] columnToPick = { 2, 11, 14, 17 }; // the size must be equals to curvesPerFigure
 
     // Curves names for each figure  /!\ size must be equals to curvesPerFigure
     public static readonly string[] loop         = { "BosseHaut", "LigneDroite", "BosseBas", "ZigZag" };
     public static readonly string[] barrelL      = { "LigneDroite", "ZigZag", "BosseBas", "LigneDroite" };
     public static readonly string[] barrelR      = { "LigneDroite", "ZagZig", "BosseBas", "LigneDroite" };
-    public static readonly string[] cubanEight   = { "DoubleBosse", "DoubleDemieLigneMontante", "DoubleZigZag", "LigneCoupee" }; // Need to be changed
+    public static readonly string[] cubanEight   = { "DoubleZigZag", "DoubleBosseHaut", "DoubleBosseBas", "DoubleZigZag" };
     public static readonly string[] straightLine = { "LigneDroite", "LigneDroite", "LigneDroite", "LigneDroite" };
 }
